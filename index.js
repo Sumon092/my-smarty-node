d1b10b7a5945e2e3fd4f1823ad9130a0bfd71994
sumon092 <m.sumon@outlook.com>
2022-04-23 02:35:10 +0600

    qurey search added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
+app.use(express.json()) // to avoid post undefined
 app.get('/', (req, res) => {
     res.send('Hello from my over smarty node');
 });
@@ -16,14 +20,33 @@ const users = [
     { id: 7, name: "Shuborna", email: "shuborna@gmail.com", phone: "01712343434" }
 ]
 
+app.get('/users', (req, res) => {
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(matched);
+    }
+    else {
+        res.send(users)
+    }
+
+})
+
 app.get('/user/:id', (req, res) => {
     console.log(req.params)
-    const id = req.params.id;
-    const user = users.find(u => u.id == id);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
 
     res.send(user);
 });
 
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = (req.body);
+    user.id = users.length + 1;
+    users.push(user)
+    res.send(user)
+})
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
